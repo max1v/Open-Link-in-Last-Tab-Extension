383f0d3f01b3c42c9b15024d510d5dfab5be94c4
Max V <45865353+max1v@users.noreply.github.com>
2026-07-24 17:39:10 -0400

    Initial commit: MV3 extension - Open link at last tab via context menu

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "openLinkAtEnd",
+    title: "Open Link at End",
+    contexts: ["link"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.menuItemId === "openLinkAtEnd" && info.linkUrl) {
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+      chrome.tabs.create({
+        url: info.linkUrl,
+        index: tabs.length
+      });
+    });
+  }
+});
